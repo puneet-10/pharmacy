59964509abcd1cf1e1629a4988e25a94849edad5
Puneet Agrawal <agrawal.puneet.10@gmail.com>
2025-05-25 07:51:51 +0000

    made the response better

--- models/medicine.go
+++ models/medicine.go
@@ -17,6 +17,17 @@ type Medicine struct {
 	UpdatedBy   string    `json:"updated_by"`
 }
 
+type MedicineDTO struct {
+	MedicineID uint   `json:"medicineId"`
+	Name       string `json:"name"`
+}
+
+type CompanyMedicinesResponse struct {
+	CompanyID   uint          `json:"companyId"`
+	CompanyName string        `json:"companyName"`
+	Medicines   []MedicineDTO `json:"medicines"`
+}
+
 // TableName overrides the default table name
 func (Medicine) TableName() string {
 	return "medicine" // This ensures GORM uses the singular "medicine" table name
@@ -94,13 +105,50 @@ func GetMedicine(id uint) (*Medicine, error) {
 }
 
 // GetAllMedicines retrieves all medicines and groups them by their associated company
-func GetAllMedicines() ([]Medicine, error) {
+//func GetAllMedicines() ([]Medicine, error) {
+//	var medicines []Medicine
+//
+//	// Fetch all medicines with company details preloaded
+//	if err := db.Preload("Company").Find(&medicines).Error; err != nil {
+//		return nil, err
+//	}
+//
+//	return medicines, nil
+//}
+
+func GetAllMedicines() ([]CompanyMedicinesResponse, error) {
 	var medicines []Medicine
 
-	// Fetch all medicines with company details preloaded
+	// Fetch medicines with associated company data
 	if err := db.Preload("Company").Find(&medicines).Error; err != nil {
 		return nil, err
 	}
 
-	return medicines, nil
+	companyMap := make(map[uint]*CompanyMedicinesResponse)
+
+	for _, med := range medicines {
+		comp := med.Company
+		if _, exists := companyMap[comp.ID]; !exists {
+			companyMap[comp.ID] = &CompanyMedicinesResponse{
+				CompanyID:   comp.ID,
+				CompanyName: comp.CompanyName,
+				Medicines:   []MedicineDTO{},
+			}
+		}
+
+		medicineDTO := MedicineDTO{
+			MedicineID: med.ID,
+			Name:       med.Name,
+		}
+
+		companyMap[comp.ID].Medicines = append(companyMap[comp.ID].Medicines, medicineDTO)
+	}
+
+	// Convert map to slice
+	var response []CompanyMedicinesResponse
+	for _, v := range companyMap {
+		response = append(response, *v)
+	}
+
+	return response, nil
 }
